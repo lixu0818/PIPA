5a88087cf6d47c9776646a2e018573e2e015a218
Yancheng Liu <yancheng102@gmail.com>
2017-03-19 17:03:04 -0400

    testing

--- pipa_webapp/pipa/instance/config.py
+++
@@ -1,9 +0,0 @@
-# instance/config.py
-
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-
-# use for local development
-SQLALCHEMY_DATABASE_URI = 'mysql://pipa_admin:pipa2017@localhost/pipa_db'
-
-# use for when running on pythonanywhere.com 
-# SQLALCHEMY_DATABASE_URI = 'mysql://your-username:your-password@your-host-address/your-database-name'
