753f3c7981d106855042fee37fb2de3a81446add
xu <lixu0818@gmail.com>
2017-03-19 17:05:39 -0400

    front end code

---
+++ pipa_webapp/pipa/app/__init__.py
@@ -0,0 +1,51 @@
+# app/__init__.py
+
+# third-party imports
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app(config_name):
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    Bootstrap(app)
+
+    from app import models
+
+    from .user import user as user_blueprint
+    app.register_blueprint(user_blueprint, url_prefix='/user')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
---
+++ pipa_webapp/pipa/app/user/forms.py
@@ -0,0 +1,16 @@
+# app/user/forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextField
+from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
+from flask_login import current_user
+
+class UserArticleForm(FlaskForm):
+    """
+    Form for user to add or edit an article
+    """
+    pmid = StringField('ID', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    abstract = TextField('Description or Abstract', validators=[DataRequired()], widget=TextArea())
+    submit = SubmitField('Submit')
